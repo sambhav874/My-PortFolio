639840fc5aaa15a2c044168345b385762e263c0c
sambhav874 <sambhavjain874@gmail.com>
2024-01-20 17:08:18 +0530

    Fixing projects issue.

--- server/routes/project.js
+++ server/routes/project.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const { createProject, getProjects } = require('../controllers/projectsController');
 
 // Get all projects
-router.get('/projects', getProjects); // Changed the route to '/projects'
+router.get('/', getProjects); // Changed the route to '/projects'
 
 // Create a new project
-router.post('/projects', createProject); // Changed the route to '/projects'
+router.post('/', createProject); // Changed the route to '/projects'
 
 module.exports = router;
