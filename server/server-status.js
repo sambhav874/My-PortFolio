b5181881da221bd1e123c737b01312e7ee348f52
sambhav874 <sambhavjain874@gmail.com>
2024-01-20 16:58:29 +0530

    Fixing projects issue.

--- server/server-status.js
+++ server/server-status.js
@@ -3,7 +3,7 @@ const contactRoutes = require('./routes/contact');
 const { sendFeedbackReply } = require('./email.sender'); // Import the email-sender logic
 
 function setupServer(app) {
-  app.use('/api/projects', projectRoute);
+  app.use('/api/projects', projectRoutes);
   app.use('/contact', contactRoutes);
 }
 
